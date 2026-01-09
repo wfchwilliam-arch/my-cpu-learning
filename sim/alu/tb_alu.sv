383c8ed5c4a1abbb2f5357791e95eded54babee4
wfchwilliam <wfchwilliam@gmail.com>
2026-01-09 21:52:20 +0800

    update: add new judgment of tb_alu.sv

--- sim/alu/tb_alu.sv
+++ sim/alu/tb_alu.sv
@@ -19,13 +19,16 @@
 import alu_pkg::*;  // 倒入alu 操作码
 
 module tb_alu;
+    // 1. 声明信号
     logic [31:0] a,b;
     alu_op_t op;
     logic [31:0] result;
     logic zero;
 
+    // 2. 实例化 alu
     alu u_alu(.*);  //自动连接
 
+    // 测试流程
     initial 
     begin
         $dumpfile("alu.vcd");
@@ -33,23 +36,57 @@ module tb_alu;
 
         // 1. 测试加法
         a = 32'd10; b = 32'd20; op =ALU_ADD;
-        #10;
-        $display("ADD : %d + %d = %d(expected 30)",a,b,result);
+        #10;// 等待电路稳定
+        // 自动检查，如果正确就告诉结果，如果错误，就报错
+        if(result != 30 )
+        begin
+            $error("ERROE: ADD Failed : %d + %d = %d(expected 30)",a,b,result);
+        end
+        else
+        begin
+            $display("ADD : %d + %d = %d(expected 30)",a,b,result);
+        end
+
 
         // 2. 测试减法
         a = 32'd10; b=32'd10; op = ALU_SUB;
         #10;
-        $display("SUB : %d - %d = %d(zero = %b expected 1)",a,b,result,zero);
+        
+        if(result != 0 )
+        begin
+            $error("ERROE: SUB Failed : %d - %d = %d(expected 30)",a,b,result);
+        end
+        else
+        begin
+            $display("SUB : %d - %d = %d(zero = %b expected 1)",a,b,result,zero);
+        end
+
 
         // 3. 测试按位与
         a = 32'hFF00FF00; b = 32'hFFFF0000; op = ALU_AND;
         #10;
-        $display("AND : %h & %h = %h(expected FF000000)",a,b,result);
+        
+        if(result != 32'hFF000000)
+        begin
+            $error("ERROE: AND Failed : %h & %h = %h(expected FF000000)",a,b,result);
+        end
+        else
+        begin
+            $display("AND : %h & %h = %h(expected FF000000)",a,b,result);
+        end
 
         // 4. 测试有符号数比大小
         a = -32'd5; b = 32'd10; op = ALU_SLT;
         #10;
-        $display("SLT : %d < %d ? result = %d (expected 1)",a, b, result);
+       
+        if(result != 1)
+        begin
+            $error("ERROE: SLT Failed :  %d < %d ? result = %d (expected 1)",a, b, result);
+        end
+        else
+        begin
+            $display("SLT : %d < %d ? result = %d (expected 1)",a, b, result);
+        end
 
     end
 
